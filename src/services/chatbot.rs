1f7a1c63eaaeed476adcf4e5069be4022ed0ecf3
Gabrielle <gabrielle.ciesla@etudiant.univ-lr.fr>
2025-12-10 21:15:25 +0100

    add some rules

--- src/services/chatbot.rs
+++ src/services/chatbot.rs
@@ -1,13 +1,51 @@
+#[derive(Debug)]
+pub enum Intent {
+    Greeting,
+    WebsiteRequest,
+    Unknown,
+}
+
+pub fn detect_intent(msg: &str) -> Intent {
+    let msg_lower = msg.to_lowercase();
+
+    if msg_lower.contains("hello") || msg_lower.contains("hi") || msg_lower.contains("salut") {
+        Intent::Greeting
+    } else if msg_lower.contains("web site") || msg_lower.contains("e-commerce") {
+        Intent::WebsiteRequest
+    } else {
+        Intent::Unknown
+    }
+}
+
+
 pub fn generate_reply(history: &Vec<String>, user_msg: &str) -> String {
-    let msg = user_msg.to_lowercase();
+    use Intent::*;
 
-    if msg.contains("bonjour") {
-        return "Salut ! Comment puis-je t’aider ?".to_string();
+    let intent = detect_intent(user_msg);
+
+    match intent {
+        Greeting => {
+            if history.is_empty() {
+                "Hi, how can I help you".to_string()
+            } else {
+                "Hy again can I help you ?".to_string()
+            }
+        }
+
+        WebsiteRequest => {
+            if history.iter().any(|m| m.to_lowercase().contains("web site")) {
+                "Have you some suggestion about youor project".to_string()
+            } else {
+                "Do you have a specific ides of your project and your price ? ".to_string()
+            }
+        }
+
+        Unknown => {
+            if history.is_empty() {
+                format!("Welcome : {}", user_msg)
+            } else {
+                format!("I didnt quit understood : {}", user_msg)
+            }
+        }
     }
-
-    if history.is_empty() {
-        return format!("Bienvenue ! Tu me dis : {}", user_msg);
-    }
-
-    format!("Tu as dit : {}", user_msg)
 }
